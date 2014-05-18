9806739cab46a358b988183f3872910d39db6a0b
King Sidharth <king.sidharth@yahoo.com>
2014-05-18 11:22:43 +0530

    Unified querry and Method to get posts with certain key>value pair

--- _plugins/category_group.rb
+++ _plugins/category_group.rb
@@ -10,6 +10,21 @@ module Jekyll
       filtered
     end
   end
+
+  module PostWithKeyValue
+    def post_with_key_value(posts, post_key, value)
+      selected = []
+      for post in posts
+        if post.data.has_key? post_key
+          if post.data.fetch(post_key) == value
+            selected.push(post)
+          end
+        end
+      end
+      selected
+    end
+  end
 end
  
 Liquid::Template.register_filter(Jekyll::ExcludePostCategory)
+Liquid::Template.register_filter(Jekyll::PostWithKeyValue)
